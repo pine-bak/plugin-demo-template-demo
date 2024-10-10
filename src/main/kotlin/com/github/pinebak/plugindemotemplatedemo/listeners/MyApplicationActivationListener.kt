7e3c19713e022e8a3434d32aa2d42324b1eb4236
GitHub Action <action@github.com>
2024-10-10 09:22:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/pinebak/plugindemotemplatedemo/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/pinebak/plugindemotemplatedemo/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pinebak.plugindemotemplatedemo.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/pinebak/plugindemotemplatedemo/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/pinebak/plugindemotemplatedemo/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.pinebak.plugindemotemplatedemo.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pinebak.plugindemotemplatedemo.MyBundle
+import com.github.pinebak.plugindemotemplatedemo.services.MyProjectService
 import javax.swing.JButton
 
 
